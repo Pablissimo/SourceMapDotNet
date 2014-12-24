0dea00e9a12fa434dfa6ee03de59266c17b33e7b
Pablissimo <turnacre@hotmail.com>
2014-12-24 21:42:46 +0000

    VlqDecoder implementation to match Mozilla

--- src/Util/VlqDecoder.cs
+++ src/Util/VlqDecoder.cs
@@ -8,43 +8,45 @@ namespace SourceMapNet.Util
 {
     public static class VlqDecoder
     {
-        private const byte MASK_CONTINUATION_BIT = 1 << 5;
-        private const byte MASK_VALUE = 0x1F;
+        private const byte VLQ_BASE_SHIFT = 5;
+        private const byte VLQ_BASE = 1 << VLQ_BASE_SHIFT;
+        private const byte VLQ_BASE_MASK = VLQ_BASE - 1;
+        private const byte VLQ_CONTINUATION_BIT = VLQ_BASE;
 
-        // Essentially a port of
-        // https://github.com/mozilla/source-map/blob/master/lib/source-map/base64-vlq.js
         public static int Decode(ref string encoded)
         {
-            int toReturn = 0;
-            var idx = 0;
+            int result = 0;
+            var encodedLength = encoded.Length;
+            var encodedIdx = 0;
             var isContinued = true;
             var shift = 0;
 
             do
             {
-                if (idx > encoded.Length)
+                if (encodedIdx >= encodedLength)
                 {
                     throw new ArgumentException("Too few characters in supplied encoded value");
                 }
 
-                var b = Base64.Decode(encoded[idx++]);
-                var value = b & MASK_VALUE;
-                toReturn = toReturn + (value << shift);
-                shift += 5;
-                isContinued = (b & MASK_CONTINUATION_BIT) == MASK_CONTINUATION_BIT;
+                var b = Base64.Decode(encoded[encodedIdx++]);
+                isContinued = (b & VLQ_CONTINUATION_BIT) == VLQ_CONTINUATION_BIT;
+
+                var digit = b & VLQ_BASE_MASK;
+                result = result + (digit << shift);
+                shift += VLQ_BASE_SHIFT;
             }
             while (isContinued);
 
-            encoded = encoded.Substring(idx);
-            
-            if ((toReturn & 0x1) == 0x1)
-            {
-                return -1 * (toReturn >> 1);
-            }
-            else
-            {
-                return toReturn >> 1;
-            }
+            encoded = encoded.Substring(encodedIdx);
+            return FromVlqSigned(result);
+        }
+
+        private static int FromVlqSigned(int signedValue)
+        {
+            var isNegative = ((signedValue & 0x1) == 0x1);
+            var shifted = signedValue >> 1;
+
+            return isNegative ? -shifted : shifted;
         }
     }
 }
