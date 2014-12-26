175a7ae1956e5298812fcb886e119c3f3537c32b
Pablissimo <turnacre@hotmail.com>
2014-12-26 19:42:21 +0000

    Description + copyright info

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SourceMapNet")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Parses version 3 source map files for very limited querying")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("pablissimo.com")]
 [assembly: AssemblyProduct("SourceMapNet")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © Paul O'Neill 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.*")]
 [assembly: InternalsVisibleTo("SourceMapDotNetTests")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
