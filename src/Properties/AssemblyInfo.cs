907654cb8522b00c5ee0daa58baa128edcb44cf2
Pablissimo <turnacre@hotmail.com>
2014-12-27 10:25:54 +0000

    Fixing assy name

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SourceMapNet")]
+[assembly: AssemblyTitle("SourceMapDotNet")]
 [assembly: AssemblyDescription("Parses version 3 source map files for very limited querying")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("pablissimo")]
